6a4098fe1acada79b56ef63ecebb77b1cfee69c8
CandyFlipper <mrjmr1337@mail.ru>
2022-06-04 19:44:35 +0800

    with-photo

--- js/header.js
+++ js/header.js
@@ -71,4 +71,31 @@ $(document).ready(function() {
   });
   
 });
+jQuery(function ($) {
+  function fix_size() {
+      var images = $('.item__img');
+      images.each(setsize);
+
+      function setsize() {
+          var img = $(this),
+              img_dom = img.get(0),
+              container = img.parents('.window__item');
+          if (img_dom.complete) {
+              resize();
+          } else img.one('load', resize);
+
+          function resize() {
+              if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
+                  img.width('100%');
+                  img.height('100%');
+                  return;
+              }
+              img.height('100%');
+              img.width('100%');
+          }
+      }
+  }
+  $(window).on('resize', fix_size);
+  fix_size();
+});
   
